003ef65e4293eca9699bff66b54b919c03f84dc2
zhou-jiahao <1836418597@qq.com>
2020-09-28 09:59:12 +0800

    周加浩完成

---
+++ demo02/src/main/java/com/kgc/zjh/controller/BookInfoController.java
@@ -0,0 +1,45 @@
+package com.kgc.zjh.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.kgc.zjh.pojo.BookInfo;
+import com.kgc.zjh.service.BookInfoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class BookInfoController {
+
+    @Resource
+    BookInfoService bookInfoService;
+
+    @RequestMapping("/BookInfo")
+    public String BookInfo(Model model,String pageIndex,
+                           @RequestParam(value = "bookType",required = false,defaultValue = "0") int bookType,
+                           @RequestParam(value = "bookName",required = false,defaultValue = "")String bookName,
+                           @RequestParam(value = "isBorrow",required = false,defaultValue = "2")int isBorrow){
+        int pageNum=1;
+        if(pageIndex!=null){
+            pageNum=Integer.parseInt(pageIndex);
+        }
+        int pageSize=3;
+
+        PageHelper.startPage(pageNum,pageSize);
+        List<BookInfo> bookInfos = bookInfoService.selectAll(bookType,bookName,isBorrow);
+        PageInfo pageInfo=new PageInfo(bookInfos);
+        model.addAttribute("pageInfo",pageInfo);
+        return "index";
+    }
+
+    @RequestMapping("/close")
+    public String close(HttpSession session){
+        session.invalidate();
+        return "redirect:/toLogin";
+    }
+}
---
+++ demo02/src/main/java/com/kgc/zjh/mapper/UsersMapper.java
@@ -0,0 +1,30 @@
+package com.kgc.zjh.mapper;
+
+import com.kgc.zjh.pojo.Users;
+import com.kgc.zjh.pojo.UsersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UsersMapper {
+    int countByExample(UsersExample example);
+
+    int deleteByExample(UsersExample example);
+
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    Users selectByPrimaryKey(Integer userId);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
